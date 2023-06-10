86b44c2043ed944b9f71d4f2d2277f4f0c745c4f
DoDoGaMaRu <101071839+DoDoGaMaRu@users.noreply.github.com>
2023-06-11 02:20:04 +0900

    temp

--- routes/review.js
+++ routes/review.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const createReview = async (review) => await model['review'].create(review);
 const selectAllReviewByMenuId = async (menu_id) => await model['review'].findAll({where: {menu_id: menu_id}});
+
 router.get('/test', function (req, res) {
     res.render('./test/.ejs');
 });
@@ -12,18 +13,29 @@ router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
+
+const selectAllDetailByOrderId = async (order_id) => await model['detail'].findAll({where: {order_id: order_id}});
+const selectUserBySn = async (sn) => await model['user'].findOne({where: {sn: sn}})
+
 router.post('/review', async (req, res, next) => {
     const order_id = req.body.order_id;
-    const user_id = req.body.user_id;
+    const details = await selectAllDetailByOrderId(order_id);
+
+    const user_sn = req.body.user_sn;
     const star = req.body.star;
     const info = req.body.info;
+    const user = await selectUserBySn(user_sn)
 
-    await createReview({
-        order_id: order_id,
-        user_id: user_id,
-        star: star,
-        info: info
-    })
+    for (const d of details) {
+        await createReview({
+            star: star,
+            info: info,
+            name: user.name,
+            user_sn: user_sn,
+            detail_id: d.id,
+            menu_id: d.menu_id,
+        })
+    }
 
     res.send();
 });
